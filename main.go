8aebfea00875b330a0ce126fa72361dfe09413cc
xuews <xuews@hsmap.com>
2021-02-25 10:55:47 +0800

    拆分上下文和路由

--- main.go
+++ main.go
@@ -15,9 +15,22 @@ func saveHandle(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	e := engine.New()
-	e.GET("search", searchHandle)
-	e.POST("save", saveHandle)
 
-	e.Run(":8888")
+	r := engine.New()
+	r.GET("/", func(c *engine.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+	r.GET("/hello", func(c *engine.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.POST("/login", func(c *engine.Context) {
+		c.JSON(http.StatusOK, map[string]string{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
+
+	r.Run(":9999")
 }
